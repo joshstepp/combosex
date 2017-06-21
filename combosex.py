61bdcad5c3798cec5d04e8f789f0b8c81682d34e
joshstepp <josh.stepp5@gmail.com>
2017-06-20 20:33:19 -0500

    updates wordlist and varible option for line numbers

--- combosex.py
+++ combosex.py
@@ -12,11 +12,14 @@ lines = open('masterlist.txt').read().splitlines()
 myline =random.choice(lines)
 myline2 =random.choice(lines)
 
+#user input for number of lines
+numberOfLines = input("How many matches would you like? ")
+
 
 #simple while loop to get 10 results
 counter = 0
 
-while (counter < 10):
+while (counter < numberOfLines):
     myline =random.choice(lines)
     myline2 =random.choice(lines)
     print (myline)+(" ")+(myline2)
